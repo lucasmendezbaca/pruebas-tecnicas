4e6d834840d3cf1117d47228da12cd8cad4ec9e5
Lucas Méndez <lucasmendezbaca@gmail.com>
2023-07-15 17:08:27 +0200

    proyecto inicializado, eslint y prettier

---
+++ pruebas/01-reading-list/lucasmendezbaca/.eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard-with-typescript',
+    'eslint-config-prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['react'],
+  rules: {
+    '@typescript-eslint/space-before-function-paren': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+  },
+}
